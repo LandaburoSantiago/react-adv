53abe9ed86f7d86e4456bcf51280099f410bb47c
santiagol <santiagol@bombieri.com.ar>
2022-12-06 20:10:13 -0300

    commit

--- src/02-components-patterns/pages/ShoppingPage.tsx
+++ src/02-components-patterns/pages/ShoppingPage.tsx
@@ -5,8 +5,6 @@ import {
   ProductTitle,
 } from "../components";
 import { products } from "../data/products";
-import { useShoppingCart } from "../hooks/useShoppingCart";
-import "../styles/custom-styles.css";
 
 const product = products[0];
 
@@ -18,7 +16,6 @@ export const ShoppingPage = () => {
       <ProductCard
         key={product.id}
         product={product}
-        className="bg-dark text-white"
         initialValues={{
           count: 4,
           maxCount: 10,
@@ -26,17 +23,9 @@ export const ShoppingPage = () => {
       >
         {({ reset, isMaxCountReached, increaseBy, count, maxCount }) => (
           <>
-            <ProductImage className="custom-image" />
-            <ProductTitle className="text-white text-bold" />
-            <ProductButtons className="custom-buttons" />
-            <button onClick={reset}>Reset</button>
-            <button onClick={() => increaseBy(-2)}>-2</button>
-            {!isMaxCountReached && (
-              <button onClick={() => increaseBy(2)}>+2</button>
-            )}
-            <span>
-              {count} - {maxCount}
-            </span>
+            <ProductImage />
+            <ProductTitle />
+            <ProductButtons />
           </>
         )}
       </ProductCard>
